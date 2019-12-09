d8e93f3242a242c6be83137d90d2419d701e4017
rmazza <robertmazza1@gmail.com>
2019-12-09 15:17:56 -0700

    add

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -12,17 +12,17 @@ export class HeroesComponent implements OnInit {
   selectedHero: Hero;
   heroes: Hero[];
 
+  constructor(private heroService: HeroService) { }
+
+  ngOnInit() {
+    this.getHeroes();
+  }
+  
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
   getHeroes(): void {
     this.heroes = this.heroService.getHeroes();
-  }
-  
-  constructor(private heroService: HeroService) { }
-
-  ngOnInit() {
-    this.getHeroes();
   }
 }
