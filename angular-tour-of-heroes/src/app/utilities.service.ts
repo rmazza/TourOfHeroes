fcd96c28555fffad4c4f3df7751ede812af39df7
robert.mazza <robert.mazza@mortgagecadence.com>
2020-03-25 07:40:10 -0600

    added utility service get guid method

--- angular-tour-of-heroes/src/app/utilities.service.ts
+++ angular-tour-of-heroes/src/app/utilities.service.ts
@@ -1,9 +1,23 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+
+import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UtilitiesService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
+
+  getGuid(): Observable<string> {
+    return of('test');
+  }
+
+  private log(message: string) {
+    this.messageService.add(`UtilitiesService: ${message}`);
+  }
 }
--- angular-tour-of-heroes/src/app/utility/utility.component.ts
+++ angular-tour-of-heroes/src/app/utility/utility.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+import { UtilitiesService } from '../utilities.service';
+
 @Component({
   selector: 'app-utility',
   templateUrl: './utility.component.html',
   styleUrls: ['./utility.component.css']
 })
 export class UtilityComponent implements OnInit {
-
-  constructor() { }
+  currentGuid: string;
+  constructor(private utilityService: UtilitiesService) { }
 
   ngOnInit() {
+    this.getNewGuid();
+  }
+
+  getNewGuid(): void {
+    this.utilityService.getGuid()
+    .subscribe(guid => this.currentGuid = guid);
   }
 
 }
